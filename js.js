aae27930e1ba9483a2576615766a00a4266b29d3
elmir14 <zhumabaevelmir@gmail.com>
2023-09-28 16:44:22 +0600

    first commit

---
+++ js.js
@@ -0,0 +1,30 @@
+//1дз
+// function factorialRecursive(n) {
+//     if (n === 0) {
+//     return 1;
+//     } else {
+//     return n * factorialRecursive(n - 1);
+//     }
+//     }
+    
+//     const number = 5;
+//     const factorial = factorialRecursive(number);
+//     console.log(`Факториал ${number} равен ${factorial}`);
+    
+//2дз
+// let str = "My name 457 is Alex"
+
+// let digits = str.match(/\D/g);
+// console.log(digits);
+
+
+//3дз
+// let str = "Hello\tWorld\nTest";
+// let whitespaceRegex = /\s/g;
+// let matches = str.match(whitespaceRegex);
+// console.log(matches);
+
+
+
+//доп
+
